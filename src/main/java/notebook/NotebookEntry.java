901bc0f9a85e3b00fe5177baf83e3bd7801e2c97
Elizabeth Kapitonova <hellokapikapi@gmail.com>
2015-11-30 01:24:50 +0300

    Initial commit

---
+++ src/main/java/notebook/NotebookEntry.java
@@ -0,0 +1,11 @@
+package notebook;
+
+/**
+ * Created by Elizaveta Kapitonova on 29.11.15.
+ */
+public class NotebookEntry {
+    String entryText;
+    public NotebookEntry(String s) {
+        entryText = s;
+    }
+}
---
+++ src/test/java/max_array/MaxArrayTest.java
@@ -0,0 +1,26 @@
+package max_array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Elizaveta Kapitonova on 29.11.15.
+ */
+public class MaxArrayTest {
+    @Test
+    public void testMaxArray() {
+        String[] a = {"1", "1", "1.2", "0.1", "0.0001", "1.1"};
+        assertEquals(2.1, MaxArray.getMax(a), 1e-9);
+    }
+    @Test
+    public void testMaxArrayClose() {
+        String[] a = {"0.00001", "0.00002", "0.000015", "0.000001", "0.000015", "0.000001"};
+        assertEquals(0.000035, MaxArray.getMax(a), 1e-9);
+    }
+    @Test
+    public void testMaxArraySimple() {
+        String[] a = {"10", "15", "14", "10", "5", "8", "10", "7"};
+        assertEquals(25, MaxArray.getMax(a), 1e-9);
+    }
+}
