173d3a212b344dc48443aa33a040157f10a200e1
Elizabeth Kapitonova <hellokapikapi@gmail.com>
2016-01-16 13:56:02 +0300

    Task 6(2)

---
+++ src/main/java/task6/properties_to_map/PropertiesToMapReader.java
@@ -0,0 +1,53 @@
+package task6.properties_to_map;
+
+import task5.universal_properties.PropertiesFileNotFoundException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * Created by Elizaveta Kapitonova on 16.01.16.
+ */
+public class PropertiesToMapReader {
+    private Properties property;
+    private Map<String, String> propertyMap;
+
+    public PropertiesToMapReader(String path)  {
+        property = new Properties();
+        try(InputStream inputStream = getClass().getClassLoader().getResourceAsStream(path)) {
+            if (inputStream!=null) {
+                property.load(inputStream);
+            } else {
+                System.err.format("Properties file %s not found%n", path);
+                throw new PropertiesFileNotFoundException();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public Map<String, String > getProperties() {
+        propertyMap = new HashMap<>(property.size());
+        for (String keys : property.stringPropertyNames()) {
+            propertyMap.put(keys, property.getProperty(keys));
+
+        }
+        return propertyMap;
+    }
+
+    public static void main(String[] args) {
+        final String PATH_ONE = "task5_properties/one.properties";
+        final String PATH_THREE_WRONG = "task5_properties/thre.properties";
+        PropertiesToMapReader propertiesToMapReader = new PropertiesToMapReader(PATH_ONE);
+        Map<String, String> map = propertiesToMapReader.getProperties();
+        for (String k : map.keySet()) {
+            System.out.println(k + " " + map.get(k));
+        }
+        PropertiesToMapReader ptmr = new PropertiesToMapReader(PATH_THREE_WRONG);
+        Map<String, String> map_wrong = propertiesToMapReader.getProperties();
+    }
+
+
+}
